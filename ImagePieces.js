1759d2a5471a390f10734a8e1470bc10a237a26f
655499 <72221990+655499@users.noreply.github.com>
2022-06-17 10:48:26 -0400

    Add files via upload

---
+++ ImagePieces.js
@@ -0,0 +1,14 @@
+export const pieceimages = {
+	'white_pawn': './images/WhitePawn.png'
+	'white_rook': './images/WhiteRook.png'
+	'white_knight': './images/WhiteKnight.png'
+	'white_bishop': './images/WhiteBishop.png'
+	'white_queen': './images/WhiteQueen.png'
+	'white_king': './images/WhiteKing.png'
+	'black_pawn': './images/BlackPawn.png'
+	'black_rook': './images/BlackRook.png'
+	'black_knight': './images/BlackKnight.png'
+	'black_bishop': './images/BlackBishop.png'
+	'black_queen': './images/BlackQueen.png'
+	'black_king': './images/BlackKing.png'
+}
